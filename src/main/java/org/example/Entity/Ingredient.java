3a0cac32334b38ab0abb103ed83b4a1590c2065a
DeliverWing <hagilashlomov@gmail.com>
2025-12-10 15:22:46 +0200

    update new classes

--- src/main/java/org/example/Entity/Ingredient.java
+++ src/main/java/org/example/Entity/Ingredient.java
@@ -1,10 +1,10 @@
 package org.example.Entity;
-
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
+
 
 @Data
 @AllArgsConstructor
@@ -13,24 +13,16 @@ import java.time.LocalDateTime;
 @Setter
 @ToString
 @Entity
-@Table(name = "Users")
-public class User {
+@Table(name = "Ingredients")
+public class Ingredient {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column(nullable = false, unique = true, length = 50)
-    private String username;
-
-    @Column(unique = true, nullable = false, length = 100)
-    private String email;
-
-    @Column(nullable = false, length = 255)
-    private String passwordHash;
+    @Column(nullable = false, unique = true, length = 100)
+    private String name;
 
     @CreationTimestamp
     @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
-
-
 }
---
+++ src/main/java/org/example/Entity/RecipeIngredient.java
@@ -0,0 +1,31 @@
+package org.example.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "RecipeIngredients")
+public class RecipeIngredient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "recipeId", nullable = false)
+    private Recipe recipeId;
+
+    @ManyToOne
+    @JoinColumn(name = "ingredientId", nullable = false)
+    private Ingredient ingredientId;
+
+    @Column(nullable = false)
+    private String quantity;
+
+    private Integer position;
+}
